cc2c92d86a7c9bfbc28fbbe02f53c32a5f6c16e0
Sabilr12 <sabil23ti@mahasiswa.pcr.ac.id>
2024-12-03 13:36:04 +0700

    Pebarui Data

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+
+@section('content')
+    <h1>Selamat Datang Di Sistem Babe Thai Tea!</h1>
+    <h5>Anda Login Sebagai Admin!</h5>
+@endsection
