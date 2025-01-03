626b97ee5badbf4e207da04723d237a3b62f769d
Zikrul Cahyadi <zikrul23ti@mahasiswa.pcr.ac.id>
2025-01-03 10:15:57 +0700

    Membuat CRUD untuk Promo

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SliderController;
+use App\Http\Controllers\PromoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,6 +15,5 @@ Route::get('/Dashboard', function () {
 });
 
 Route::resource('menu', MenuController::class);
-
-
 Route::resource('slider', SliderController::class);
+Route::resource('promo', PromoController::class);
